1338b0289668f67d3a261d56a17464c41e35290e
Jason Hsu <jhsu@debian.debianserver.com>
2013-04-10 17:39:59 -0500

    Remove newline in string_from_file

--- scrape.rb
+++ scrape.rb
@@ -410,7 +410,7 @@ end
 # Thanks to Keith R. Bennett for suggesting this solution on ruby.mn
 def string_from_file (filename)
   str_output = File.readlines(filename).join('')
-  str_output = str_output.gsub(",", "")
+  str_output = str_output.gsub("\n", "")
   return str_output
 end
 
